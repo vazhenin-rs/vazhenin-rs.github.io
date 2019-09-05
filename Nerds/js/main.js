ed55a2da1299ab4652d556e4638ed95dcadd4d64
vazhenin-rs <vazhenin.rs93@gmail.com>
2019-09-05 13:08:11 +0600

    Загрузил все файлы проекта

---
+++ Nerds/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      arrows: false,
+      swipe: false,
+      dots: true,
+      dotsClass: 'slider-dots-custom',
+      autoplay: true,
+      autoplaySpeed: 5000
+   });
+});
